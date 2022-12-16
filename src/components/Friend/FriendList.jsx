4f6d49f396cacbd5caac29f8f6f630949fd2f593
YanaLiz <105737326+YanaLiz@users.noreply.github.com>
2022-12-16 16:51:40 +0200

    add

--- src/components/Friend/FriendList.jsx
+++ src/components/Friend/FriendList.jsx
@@ -13,10 +13,10 @@ export const FriendList = ({friends}) => {
             id={id}
             name={name}
             avatar={avatar}
-            // isOnline={isOnline}
+            isOnline={isOnline}
           >
           
-  {/* <span className={isOnline?css.status_online:css.status_offline}>{isOnline}</span> */}
+  <span className={isOnline?css.status_online:css.status_offline}>{isOnline}</span>
   <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
   <p className={css.name}>{name}</p>
 </li>
@@ -30,7 +30,7 @@ FriendList.propTypes = {
   props: PropTypes.arrayOf(PropTypes.exact({
     avatar: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  // isOnline: PropTypes.bool.isRequired,
+  isOnline: PropTypes.bool.isRequired,
   id:PropTypes.number.isRequired
   }))
 
